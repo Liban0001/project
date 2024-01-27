4b70263077e2af9109dc94c1bed6d40517faa8e4
Liban Ahmed <LAhmed-SEC69-J@bootcamp.firebrand.training>
2024-01-27 12:15:07 +0000

    initial commit

---
+++ tests/CalculatorTest.java
@@ -0,0 +1,12 @@
+package tests;
+
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+
+public class CalculatorTest {
+    @Test
+    public void testAdd() {
+        Calculator calculator = new Calculator();
+        assertEquals(3, calculator.add(1, 2));
+    }
+}
