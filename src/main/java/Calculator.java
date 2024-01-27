912604013d3113b75c70a1f30d2e6ccbd67f42c6
Liban Ahmed <LAhmed-SEC69-J@bootcamp.firebrand.training>
2024-01-27 14:58:46 +0000

    teach brother about ci pipelines

--- src/main/java/Calculator.java
+++ src/main/java/Calculator.java
@@ -1,7 +1,11 @@
+import java.util.*;
 
 
 public class Calculator {
-    public int add(int b, int c) {
-        return b * c;
+    public int add(int a, int b) {
+        return a * b;
+
     }
+
 }
+
