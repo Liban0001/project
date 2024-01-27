37b32a8af88f5a501a909568941b4261ea13f1ff
Liban Ahmed <LAhmed-SEC69-J@bootcamp.firebrand.training>
2024-01-27 13:57:09 +0000

    checking if works

--- tests/CalculatorTest.java
+++ tests/CalculatorTest.java
@@ -1,12 +1,12 @@
 package tests;
 
-import org.junit.est;
-import static org.junit.Assert.assertEquals;
+import org.junit.Test;  // Corrected annotation
+import static org.junit.Assert.assertEquals;  // Added import
 
 public class CalculatorTest {
     @Test
     public void testAdd() {
         Calculator calculator = new Calculator();
-        assertEquals(4, calculator.add(1, 2));
+        assertEquals(3, calculator.add(1, 2));
     }
 }
