49675a4040992c36d89f69c42d545dfcbb87acf0
Liban Ahmed <LAhmed-SEC69-J@bootcamp.firebrand.training>
2024-01-28 16:38:20 +0000

    Added subtraction unit testing

--- src/main/java/Calculator.java
+++ src/main/java/Calculator.java
@@ -2,9 +2,14 @@ import java.util.*;
 
 
 public class Calculator {
+
     public int add(int a, int b) {
         return a + b;
 
+    }
+
+    public int subtract(int a, int b){
+        return a - b;
     }
 
 }
--- src/test/java/CalculatorTest.java
+++ src/test/java/CalculatorTest.java
@@ -1,12 +1,16 @@
 
-
 import org.junit.Test;  // Corrected annotation
 import static org.junit.Assert.assertEquals;  // Added import
 
 public class CalculatorTest {
+    Calculator calculator = new Calculator();
+    
     @Test
     public void testAdd() {
-        Calculator calculator = new Calculator();
         assertEquals(3, calculator.add(1, 2));
+    }
+    @Test
+    public void testSubtract() {
+        assertEquals(2, calculator.subtract(5, 3));
     }
 }
